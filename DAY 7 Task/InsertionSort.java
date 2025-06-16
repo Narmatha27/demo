088ce5d5033e3cae528f12ba235962388f533c0e
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-16 10:49:29 +0530

    finished

---
+++ DAY 7 Task/InsertionSort.java
@@ -0,0 +1,43 @@
+package addon_dsa;
+
+public class InsertionSort {
+
+	    
+	    public static void insertionSort(int[] arr) {
+	        for (int i = 1; i < arr.length; i++) {
+	            int key = arr[i];
+	            int j = i - 1;
+
+	            
+	            while (j >= 0 && arr[j] > key) {
+	                arr[j + 1] = arr[j];
+	                j--;
+	            }
+
+	            arr[j + 1] = key;
+	        }
+	    }
+
+	   
+	    public static void printArray(int[] arr) {
+	        for (int value : arr) {
+	            System.out.print(value + " ");
+	        }
+	        System.out.println();
+	    }
+
+	   
+	    public static void main(String[] args) {
+	        int[] arr = {5, 2, 9, 1, 5, 6};
+
+	        System.out.println("Original array:");
+	        printArray(arr);
+
+	        insertionSort(arr);
+
+	        System.out.println("Sorted array:");
+	        printArray(arr);
+	    }
+	}
+
+
